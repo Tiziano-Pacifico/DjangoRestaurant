f9d23786557e3de9ba2c1a59ee588369997b5d3c
Tiziano Pacifico <gourmetmolise@gmail.com>
2025-03-25 12:48:55 +0100

    vendor approval by admin, vendor dashboard with cards

--- accounts/util.py
+++ accounts/util.py
@@ -20,16 +20,30 @@ def detectUser(user):
     
 def send_verification_email(request, user, mail_subject,email_template):
 
+    print(mail_subject)
     from_email = settings.DEFAULT_FROM_EMAIL
+    print(from_email)
     current_site = get_current_site(request)
+    print(current_site)
     message = render_to_string(email_template, {
         'user':user,
         'domain': current_site,
         'uid': urlsafe_base64_encode(force_bytes(user.pk)),
         'token': default_token_generator.make_token(user),
     })
-    print('sono qui')
+    print(message)
     to_email = user.email
+    print(to_email)
     mail = EmailMessage(mail_subject, message, from_email,to=[to_email])
     mail.send()
+
+
+def send_notification(mail_subject, mail_template, context):
+    from_email = settings.DEFAULT_FROM_EMAIL
+    message = render_to_string(mail_template, context)
+    to_email = context['user'].email
+    print(from_email)
+    mail = EmailMessage(mail_subject, message, from_email,to=[to_email])
+    mail.send()
+
 
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 
 from accounts.util import detectUser, send_verification_email
 from vendor.forms import VendorForm
+from vendor.models import Vendor
 from .forms import UserForm
 from .models import User, UserProfile
 from django.contrib import messages, auth
